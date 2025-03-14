76a47c5ca2cd6a2691975cb8311d352b86ceffdc
Karl Friebel <karl.friebel@tu-dresden.de>
2025-03-14 09:44:19 +0100

    Added lifting patterns.

--- lib/Dialect/EKL/Transforms/Lift.cpp
+++ lib/Dialect/EKL/Transforms/Lift.cpp
@@ -21,11 +21,14 @@
 
 #include "llvm/Support/Debug.h"
 
+#include <algorithm>
 #include <llvm/ADT/STLExtras.h>
 #include <llvm/ADT/Sequence.h>
+#include <llvm/ADT/SmallVector.h>
 #include <llvm/ADT/TypeSwitch.h>
 #include <llvm/Support/LogicalResult.h>
 #include <mlir/IR/OpDefinition.h>
+#include <mlir/IR/Value.h>
 #include <mlir/IR/Visitors.h>
 
 using namespace mlir;
@@ -87,7 +90,7 @@ struct SplitReduction : OpRewritePattern<ReduceOp> {
         rewriter.moveOpBefore(
             source,
             axes.back().getMap(),
-            axes.back().getMap()->end());
+            axes.back().getMap()->begin());
         rewriter.modifyOpInPlace(source, [&]() {
             for (unsigned dim = 0; dim < numDims - 1U; ++dim) {
                 rewriter.replaceAllUsesWith(
@@ -330,6 +333,184 @@ struct DistributeFactor : OpRewritePattern<ReduceOp> {
     }
 };
 
+static AssocOp findTop(AssocOp bottom, Region *definition)
+{
+    auto top = bottom;
+    for (auto next = top->getParentOfType<AssocOp>();
+         next && definition->isAncestor(next->getParentRegion());
+         next = next->getParentOfType<AssocOp>())
+        top = next;
+
+    return top;
+}
+
+static FailureOr<std::pair<AssocOp, bool>>
+prepareLiftAssoc(AssocOp assoc, SmallVectorImpl<BlockArgument> &indices)
+{
+    AssocOp top{};
+    for (auto parent = assoc->getParentOfType<AssocOp>(); parent;
+         parent      = parent->getParentOfType<AssocOp>()) {
+        const auto args = llvm::reverse(parent.getMap()->getArguments());
+        indices.insert(indices.end(), args.begin(), args.end());
+        top = parent;
+    }
+    if (!top) return failure();
+
+    DenseSet<BlockArgument> usedIndices;
+    assoc->walk([&](Operation *op) {
+        for (auto opd : op->getOperands()) {
+            if (auto arg = llvm::dyn_cast<BlockArgument>(opd))
+                if (llvm::isa<AssocOp>(arg.getOwner()->getParentOp())) {
+                    usedIndices.insert(arg);
+                    continue;
+                }
+
+            const auto definition = opd.getParentRegion();
+            if (assoc.getMapRegion().isAncestor(definition)
+                || definition->isAncestor(top->getParentRegion()))
+                continue;
+
+            top = findTop(assoc, definition);
+        }
+
+        if (op->hasTrait<mlir::OpTrait::IsIsolatedFromAbove>())
+            return WalkResult::skip();
+        return WalkResult::advance();
+    });
+    if (top == assoc) return failure();
+
+    const auto numIndices = indices.size();
+    llvm::remove_if(indices, [&](BlockArgument arg) {
+        return !usedIndices.contains(arg);
+    });
+    std::reverse(indices.begin(), indices.end());
+
+    return std::make_pair(top, indices.size() < numIndices);
+}
+
+struct LiftAssoc : OpRewritePattern<AssocOp> {
+    using OpRewritePattern::OpRewritePattern;
+
+    LogicalResult
+    matchAndRewrite(AssocOp op, PatternRewriter &rewriter) const final
+    {
+        SmallVector<BlockArgument> indices;
+        const auto maybeResult = prepareLiftAssoc(op, indices);
+        if (failed(maybeResult) || !maybeResult->second) return failure();
+
+        const auto arrayTy = llvm::cast<ArrayType>(getTypeBound(op.getType()));
+        SmallVector<extent_t> extents;
+        for (auto arg : indices) {
+            const auto indexTy =
+                llvm::dyn_cast<ekl::IndexType>(getTypeBound(arg));
+            if (!indexTy) return failure();
+            extents.push_back(indexTy.getUpperBound() + 1U);
+        }
+        concat(extents, arrayTy.getExtents());
+
+        rewriter.setInsertionPoint(maybeResult->first);
+        auto lifted = rewriter.create<AssocOp>(
+            op.getLoc(),
+            ArrayType::get(arrayTy.getScalarType(), extents));
+
+        rewriter.setInsertionPointAfter(op);
+        auto subscript = rewriter.create<SubscriptOp>(
+            op.getLoc(),
+            lifted.getResult(),
+            ValueRange(indices),
+            getTypeBound(op.getType()));
+        rewriter.replaceAllUsesWith(op.getResult(), subscript.getResult());
+
+        rewriter.moveOpBefore(op, lifted.getMap(), lifted.getMap()->begin());
+        for (auto [i, idx] : llvm::enumerate(indices))
+            rewriter.replaceUsesWithIf(
+                idx,
+                lifted.getMap()->getArgument(i),
+                [&](OpOperand &use) {
+                    return lifted->isAncestor(use.getOwner());
+                });
+
+        rewriter.setInsertionPointToEnd(lifted.getMap());
+        rewriter.create<YieldOp>(op.getLoc(), op.getResult());
+        return success();
+    }
+};
+
+struct LiftReduce : OpRewritePattern<ReduceOp> {
+    using OpRewritePattern::OpRewritePattern;
+
+    LogicalResult
+    matchAndRewrite(ReduceOp reduce, PatternRewriter &rewriter) const final
+    {
+        auto source = reduce.getArray().getDefiningOp<AssocOp>();
+        if (!source || !source->hasOneUse()) return failure();
+
+        SmallVector<BlockArgument> indices;
+        const auto maybeResult = prepareLiftAssoc(source, indices);
+        if (failed(maybeResult)) return failure();
+
+        auto top = maybeResult->first;
+        reduce->walk([&](Operation *op) {
+            for (auto opd : op->getOperands()) {
+                const auto definition = opd.getParentRegion();
+                if (reduce.getReductionRegion().isAncestor(definition)
+                    || definition->isAncestor(top->getParentRegion()))
+                    continue;
+
+                top = findTop(source, definition);
+            }
+
+            if (op->hasTrait<mlir::OpTrait::IsIsolatedFromAbove>())
+                return WalkResult::skip();
+            return WalkResult::advance();
+        });
+        if (top == source) return failure();
+
+        if (llvm::equal(top.getMap()->getArguments(), indices))
+            return failure();
+
+        const auto scalarTy =
+            llvm::cast<ScalarType>(getTypeBound(reduce.getType()));
+        SmallVector<extent_t> extents;
+        for (auto arg : indices) {
+            const auto indexTy =
+                llvm::dyn_cast<ekl::IndexType>(getTypeBound(arg));
+            if (!indexTy) return failure();
+            extents.push_back(indexTy.getUpperBound() + 1U);
+        }
+
+        rewriter.setInsertionPoint(top);
+        auto lifted = rewriter.create<AssocOp>(
+            reduce.getLoc(),
+            ArrayType::get(scalarTy, extents));
+
+        rewriter.setInsertionPointAfter(reduce);
+        auto subscript = rewriter.create<SubscriptOp>(
+            reduce.getLoc(),
+            lifted.getResult(),
+            ValueRange(indices),
+            scalarTy);
+        rewriter.replaceAllUsesWith(reduce.getResult(), subscript.getResult());
+
+        rewriter.moveOpBefore(
+            source,
+            lifted.getMap(),
+            lifted.getMap()->begin());
+        for (auto [i, idx] : llvm::enumerate(indices))
+            rewriter.replaceUsesWithIf(
+                idx,
+                lifted.getMap()->getArgument(i),
+                [&](OpOperand &use) {
+                    return lifted->isAncestor(use.getOwner());
+                });
+
+        rewriter.moveOpAfter(reduce, source);
+        rewriter.setInsertionPointToEnd(lifted.getMap());
+        rewriter.create<YieldOp>(reduce.getLoc(), reduce.getResult());
+        return success();
+    }
+};
+
 } // namespace
 
 void mlir::ekl::populateLiftPatterns(RewritePatternSet &patterns)
@@ -339,6 +520,8 @@ void mlir::ekl::populateLiftPatterns(RewritePatternSet &patterns)
     patterns.add<SplitReduction>(patterns.getContext());
 
     patterns.add<LiftFactor, DistributeFactor>(patterns.getContext());
+
+    patterns.add<LiftAssoc, LiftReduce>(patterns.getContext());
 }
 
 //===----------------------------------------------------------------------===//
